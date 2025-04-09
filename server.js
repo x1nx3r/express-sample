afc26e548f1c476003e72d50d8a97e80cf365b42
Irsyad Firmansyah <irsyadjelek35@gmail.com>
2025-04-10 00:22:29 +0700

    bismilah

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send("anjay");
+  response.send("anjay mabar");
 })
 
 const PORT = process.env.PORT || 3000;
